076cafc39d4fae4f7a3888b6f6e7b7c288dbb1c8
edvard01 <frejaclueless@gmail.com>
2024-11-20 13:32:40 +0100

    Added vue to the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { createApp } from "vue";
+import Counter from "./components/Counter.vue";
+
+const app = createApp();
+
+app.component("counter", Counter);
+
+app.mount("#app");
